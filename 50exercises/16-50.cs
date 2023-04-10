766e12f47f6f4373791c3cdba54ca08b1e2f5417
Pedro Maia <pedroasmaia@outlook.com>
2023-04-09 22:02:30 -0300

    Correction 16-50

--- 50exercises/16-50.cs
+++ 50exercises/16-50.cs
@@ -4,3 +4,16 @@
 * Calcule e screva se os valores formam ou não um triangulo.
 * O valor de cada lado deve ser menor que o a soma dos outros dois lados.
 */
+Console.Write("Escreva  a metragem do lado A: ");
+int sideA = int.Parse(Console.ReadLine());
+Console.Write("Escreva a metragem do lado B: ");
+int sideB = int.Parse(Console.ReadLine());
+Console.Write("Escreva  a metragem do lado C: ");
+int sideC = int.Parse(Console.ReadLine());
+
+if(sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB){
+   Console.WriteLine("Forma o triangulo");
+}
+else{
+   Console.WriteLine("Não forma o triangulo");
+}
