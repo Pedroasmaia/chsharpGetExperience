8fc9855ee48231b894eb7dcf256fa282f705d7f4
Pedro Maia <pedroasmaia@outlook.com>
2023-04-10 22:55:30 -0300

    Finished 19-50 | 20-50

---
+++ 50exercises/19-50.cs
@@ -0,0 +1,16 @@
+//Todo
+/*
+* Escreva um algoritimo para ler 2 valores e se o segundo valor informado for 0
+* deve ser lido um novo valor e informar valor "Invalido", e imprimir o resultado do primeiro valor pelo segundo
+*/
+double divisor = 0;
+Console.Write("Insira o dividendo:");
+double dividend = double.Parse(Console.ReadLine());
+Console.Write("Insira o divisor:");
+divisor = double.Parse(Console.ReadLine());
+for (double i = 0; divisor == 0;)
+{
+   Console.WriteLine("Valor invalido, insira um novo valor:");
+   divisor = double.Parse(Console.ReadLine());
+}
+Console.WriteLine(dividend / divisor);
---
+++ 50exercises/20-50.cs
@@ -0,0 +1,7 @@
+//Todo
+/*
+* Escreva um algoritmo para imprimir os números de 1 a 10 em ordem crescente
+*/
+for(int i = 1;i<=10;i++){
+   Console.WriteLine(i);
+}
